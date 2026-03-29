9327db6e1aa481b6040e98d536f4154a5c2fb4c6
Sumit Sharma <sumitodeliya@gmail.com>
2026-03-29 11:59:50 +0530

    making routes

---
+++ client/app/contact/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+   <>
+   <div  className='h-screen w-full bg-amber-400'>
+    <h1>hloo about</h1>
+   
+   </div>
+
+   <div className='h-screen w-full bg-blue-400'>
+    <h1>hloo about</h1>
+   
+   </div>
+   
+   </>
+  )
+}
+
+export default contact
---
+++ client/app/price/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-full w-full bg-amber-950'>
+        <h1>welcome to price section</h1>
+        <h1>welcome to price section</h1>
+        <h1>welcome to price section</h1>
+        <h1>welcome to price section</h1>
+        <h1>welcome to price section</h1>
+        <h1>welcome to price section</h1>
+    </div>
+  )
+}
+
+export default page
